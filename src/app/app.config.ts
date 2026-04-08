5a3d658678bb5feeb1139894959f3cec0eb4db20
Piotr Deja <piotrdeja.pl@gmail.com>
2026-04-08 19:53:30 +0200

    Add ngrx store, effects and devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideEffects([]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
 };
